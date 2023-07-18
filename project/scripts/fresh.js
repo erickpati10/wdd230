4f8e6232c45ffe8a6c7d933b5f54af055462201f
erickpati10 <pat21018@byui.edu>
2023-07-18 00:52:27 -0600

    updates

---
+++ project/scripts/fresh.js
@@ -0,0 +1,100 @@
+let fruitData; // Declare the 'fruitData' variable in the global scope
+
+// Function to populate the select elements with the available fruits
+async function populateFruitsSelect() {
+  try {
+    const response = await fetch(
+      "https://brotherblazzard.github.io/canvas-content/fruit.json"
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch fruit data.");
+    }
+    fruitData = await response.json(); // Assign the fetched data to the global 'fruitData' variable
+
+    if (!Array.isArray(fruitData)) {
+      throw new Error("Invalid fruit data format.");
+    }
+
+    const fruitsSelects = document.querySelectorAll('select[id^="fruit"]');
+    fruitsSelects.forEach((select) => {
+      // Clear existing options
+      select.innerHTML = "";
+
+      // Add "Select Fruit" option as the first choice
+      const selectOption = document.createElement("option");
+      selectOption.value = ""; // Set the value of the empty option to an empty string
+      selectOption.textContent = "- Select Fruit -"; // Display text for the empty option
+      select.appendChild(selectOption);
+
+      // Populate select with available fruits
+      fruitData.forEach((fruit) => {
+        const option = document.createElement("option");
+        option.value = fruit.name;
+        option.textContent = fruit.name;
+        select.appendChild(option);
+      });
+    });
+  } catch (error) {
+    console.error("Error fetching fruit data:", error);
+  }
+}
+
+// Function to handle form submission and display order details
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const firstName = document.getElementById("first-name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const fruit1 = document.getElementById("fruit1").value;
+  const fruit2 = document.getElementById("fruit2").value;
+  const fruit3 = document.getElementById("fruit3").value;
+  const specialInstructions = document.getElementById(
+    "special-instructions"
+  ).value;
+
+  const selectedFruits = [fruit1, fruit2, fruit3];
+  let totalCarbs = 0;
+  let totalProtein = 0;
+  let totalFat = 0;
+  let totalSugar = 0;
+  let totalCalories = 0;
+
+  selectedFruits.forEach((fruit) => {
+    const selectedFruit = fruitData.find((item) => item.name === fruit);
+    if (selectedFruit) {
+      totalCarbs += Math.round(selectedFruit.nutritions.carbohydrates);
+      totalProtein += Math.round(selectedFruit.nutritions.protein);
+      totalFat += Math.round(selectedFruit.nutritions.fat);
+      totalSugar += Math.round(selectedFruit.nutritions.sugar);
+      totalCalories += Math.round(selectedFruit.nutritions.calories);
+    }
+  });
+
+  // Get the current date
+  const currentDate = new Date().toLocaleDateString("en-US");
+
+  const orderDetails = document.getElementById("order-details");
+  orderDetails.innerHTML = `
+        <h2>Order Details</h2>
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone Number:</strong> ${phone}</p>
+        <p><strong>Fruit 1:</strong> ${fruit1}</p>
+        <p><strong>Fruit 2:</strong> ${fruit2}</p>
+        <p><strong>Fruit 3:</strong> ${fruit3}</p>
+        <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
+        <h3>Nutritional Information</h3>
+        <p><strong>Total Carbohydrates:</strong> ${totalCarbs}g</p>
+        <p><strong>Total Protein:</strong> ${totalProtein}g</p>
+        <p><strong>Total Fat:</strong> ${totalFat}g</p>
+        <p><strong>Total Sugar:</strong> ${totalSugar}g</p>
+        <p><strong>Total Calories:</strong> ${totalCalories} kcal</p>
+        <p><strong>Order Date:</strong> ${currentDate}</p>
+      `;
+}
+
+const form = document.getElementById("drink-form");
+form.addEventListener("submit", handleFormSubmit);
+
+populateFruitsSelect();
