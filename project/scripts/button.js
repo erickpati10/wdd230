dac2a8a437b8ab9a60d6123d404c15b358a2dcdf
erickpati10 <pat21018@byui.edu>
2023-07-22 23:14:41 -0600

    updates

--- project/scripts/button.js
+++ project/scripts/button.js
@@ -1,13 +1,3 @@
-// Function to toggle the mobile menu
-// function toggleMobileMenu() {
-//   const mobileMenu = document.getElementById("mobile-menu");
-//   mobileMenu.classList.toggle("open");
-// }
-
-// Add an event listener to the mobile menu button
-// const mobileMenuButton = document.getElementById("mobile-menu-button");
-// mobileMenuButton.addEventListener("click", toggleMobileMenu);
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
